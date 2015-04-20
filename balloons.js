3f38aad6239e450d5ea2c493884c362bbd8c051d
Jonathan Skårstedt <jonathan.skarstedt@gmail.com>
2015-04-20 11:49:49 +0200

    Made sure balloons can't intersect

--- balloons.js
+++ balloons.js
@@ -22,10 +22,25 @@ var Balloon = function(x,y, color, radius) {
     this.radius = radius;
 }
 
+Balloon.prototype.maxToIntersect = function(nextBalloon) {
+    return Math.sqrt(Math.pow(this.x - nextBalloon.x, 2) +
+		     Math.pow(this.y - nextBalloon.y, 2))
+	- nextBalloon.radius;
+
+}
+
+Balloon.prototype.hasInside = function(nextBalloon) {
+    var distance = Math.sqrt(Math.pow(this.x - nextBalloon.x, 2) +
+			     Math.pow(this.y - nextBalloon.y, 2));
+    console.debug(this, nextBalloon,
+		  distance, this.radius, distance < this.radius);
+    return distance < this.radius; 
+}
+ 
 Balloon.prototype.draw = function() {
     context.beginPath();
-    context.arc(this.x + 360,
-		this.y + 360,
+    context.arc(this.x,
+		this.y,
 		this.radius,
 		0,
 		2 * Math.PI,
@@ -40,7 +55,6 @@ Balloon.prototype.draw = function() {
 /**
  * Methods for drawing balloons and zap-o-matic trajectory on canvas
  */
-
 var BalloonCanvas = function(canvas) {
     this.balloons = [];
     context = canvas.getContext("2d");
@@ -48,6 +62,8 @@ var BalloonCanvas = function(canvas) {
     this.state = AIMING;
     this.trajectory = {x: 0, y: 0};
     var b = this;
+    
+    // shoot or finish creating a balloon
     canvas.onclick = function(e) {
 	if(b.state === AIMING) {
 	    b.createBalloon(e.layerX, e.layerY);
@@ -56,6 +72,7 @@ var BalloonCanvas = function(canvas) {
 	}
     }
 
+    // aim or create a balloon
     canvas.onmousemove = function(e) {
 	if(b.state === AIMING) {
 	    b.trajectory.x = e.layerX;
@@ -67,38 +84,67 @@ var BalloonCanvas = function(canvas) {
     }
 }
 
+/**
+ * Clear drawing board
+ */
 BalloonCanvas.prototype.clear = function() {
     context.clearRect(0,0,this.canvas.width, this.canvas.height);
 }
 
+/**
+ * Set radius of new balloon 
+ */
 BalloonCanvas.prototype.setRadius = function() {
+    this.balloons.push(this.newBalloon)
+    this.newBalloon = null;
     this.clear();
-    this.balloons.push(this.newBalloon.toBalloon())
-    console.debug(this.newBalloon);
+    this.drawBalloons();
     this.state = AIMING;
 }
 
+/**
+ * Draw the entire canvas
+ */
 BalloonCanvas.prototype.draw = function() {
     this.clear();
     this.drawBalloons();
     this.drawTrajectory();
-    console.debug("wat");
 }
 
+/**
+ * Draw all balloons
+ */
 BalloonCanvas.prototype.drawBalloons = function() {
     for(b in this.balloons) {
 	this.balloons[b].draw()
     }
 }
 
+/**
+ * Find max intersect of balloons, ie how large the argument balloon can be
+ * before it intersect with other balloons
+ */
+BalloonCanvas.prototype.findMaxIntersect = function(balloon) {
+    var mi = Infinity;
+    for(b in this.balloons) {
+	var bmi = balloon.maxToIntersect(this.balloons[b]);
+	if(bmi < mi)
+	    mi = bmi;
+    }
+
+    return mi;
+}
+
+/**
+ * Start creating a new balloon
+ */
 BalloonCanvas.prototype.createBalloon = function(x,y) {
-    this.state = CREATION;
     var generateColor = function() {
-	// Random random = new Random();
 	var mix = {
 	    "red": 255,
 	    "green": 255,
-	    "blue": 255}
+	    "blue": 255
+	}
 	
 	var red = Math.floor(Math.random() * 256);
 	var green = Math.floor(Math.random() * 256);
@@ -113,30 +159,43 @@ BalloonCanvas.prototype.createBalloon = function(x,y) {
 
 	return "rgb(" +red+ ", " +green+ ", " +blue+ ")";
     }
-    this.newBalloon = {
-	"x": x,
-	"y": y,
-	"color": generateColor(),
-	"radius": 0,
-	"toBalloon": function() {
-	    return new Balloon(this.x - 360, this.y - 360,
-			       this.color, this.radius);
+    
+    this.newBalloon = new Balloon(x, y, generateColor(), 0);
+
+    for(b in this.balloons) {
+	if(this.balloons[b].hasInside(this.newBalloon)) {
+	    alert("There's already a balloon there!");
+	    this.newBalloon = null;
+	    return;
 	}
     }
 
+    this.state = CREATION;
+    this.newBalloon.maxRadius = this.findMaxIntersect(this.newBalloon);
+    console.debug(this.newBalloon);
 }
 
+/**
+ * Draws the new balloon, according to the current radius
+ */
 BalloonCanvas.prototype.drawNewBalloon = function(x,y) {
     this.newBalloon.radius =
 	Math.sqrt(Math.pow(this.newBalloon.x - x,2)
 		  + Math.pow(this.newBalloon.y - y, 2));
+    
+    if(this.newBalloon.radius > this.newBalloon.maxRadius) {
+	this.newBalloon.radius = Math.floor(this.newBalloon.maxRadius);
+    }
+    
     this.clear();
     this.drawBalloons();
-    this.newBalloon.toBalloon().draw();
+    this.newBalloon.draw();
 }
 
+/**
+ * Draws a trajectory for the zap-o-matic
+ */
 BalloonCanvas.prototype.drawTrajectory = function() {
-    
     this.showXY(this.trajectory.x, this.trajectory.y);
     context.beginPath();
     context.moveTo(360,360);
@@ -146,7 +205,10 @@ BalloonCanvas.prototype.drawTrajectory = function() {
     context.stroke();
 }
 
+/**
+ * Show x and y coordinates
+ */
 BalloonCanvas.prototype.showXY = function(x,y) {
-    document.getElementById("xpos").innerText = x -360;
-    document.getElementById("ypos").innerText = y -360;
+    document.getElementById("xpos").innerText = x;
+    document.getElementById("ypos").innerText = y;
 }
